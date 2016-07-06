43f2b122ba3ef2bf9b1cb3008d8fe9a01ce6bd0f
kk19ag <kk.agrawal19@gmail.com>
2016-07-06 00:19:41 -0700

    addde ifelse

---
+++ ifelse.java
@@ -0,0 +1,1 @@
+public class IfElse { public static void main(String[] args) { int age = 21; if (age <= 18) { System.out.println("User is 18 or younger"); } else if (age < 40) { System.out.println("User is between 19 & 39"); } else { System.out.println("User is above 39"); } } } 
