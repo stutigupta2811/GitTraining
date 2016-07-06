0746910aa0660b4dc0c46d26af9315a84f3c03fd
Amita Shukla <amitashukla0906@gmail.com>
2016-07-06 12:49:19 +0530

    add file

---
+++ ForEachLoop.java
@@ -0,0 +1,5 @@
+
+
+ForEachLoop
+
+public class ForEachLoop { public static void main(String[] args) { String [] names ={"James", "Larry", "Tom", "Lacy"}; for( String name : names ) { System.out.println( name ); } } }
